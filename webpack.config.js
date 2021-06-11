43e47b9d70ed9d982abe611ae09f06958ee4e498
saqfish <saqfish@gmail.com>
2021-06-11 15:29:07 -0500

    file-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
   resolve: {
